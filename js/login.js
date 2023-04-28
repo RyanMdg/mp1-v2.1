46f6b2c47a8c764213e1c2d8563d31e4295d2e3e
RyanMdg <ryandeguia.dev@gmail.com>
2023-04-28 15:12:23 +0800

    a

--- js/login.js
+++ js/login.js
@@ -2,3 +2,16 @@
 const btnLogin = document.querySelector(".btnLogin");
 const signup = document.querySelector("#typeEmail");
 const login = document.querySelector(".input-login");
+
+let cart = document.querySelector(".cart");
+const btnCart = document.querySelectorAll(".cart-btn");
+
+var count = "";
+cart.textContent = count;
+
+const handleIncrement = () => {
+  count++;
+  cart.textContent = count;
+};
+for (let i = 0; i < btnCart.length; i++)
+  btnCart[i].addEventListener("click", handleIncrement);
